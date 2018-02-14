e4d1c41b5e7cb63e1ceb56e5d96fff567b89f155
Chris Lamb <chris.lamb@gmail.com>
2018-02-13 19:05:44 -0800

    Steps 1-6

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,19 @@
 const {assert} = require('chai');
+
+describe("User visits landing page", () => { 
+	describe("visits empty landing page", () => { 
+		it("should be empty", () => {
+			browser.url("/");
+
+			assert.isEmpty(browser.getText("#videos-container"));
+		});
+		
+		it("can navigate to the create page", () => { 
+			browser.url("/");
+
+			browser.click("a[href='videos/create.html']");
+
+			assert.include(browser.getText("#page-title"), "Save a video");
+		});
+	});
+});
